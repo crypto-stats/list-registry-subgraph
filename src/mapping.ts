dc99fa9ebf6b1cb38e66d166126127d2f07e9f3b
David Mihal <david@dmihal.com>
2021-11-18 15:27:44 +0000

    Minimal subgraph tracks lists

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,3 +1,10 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { Registry } from "../generated/Registry/Registry"
-import { ExampleEntity } from "../generated/schema"
+// import { BigInt } from "@graphprotocol/graph-ts"
+import { ListCreated } from "../generated/Registry/Registry"
+import { List } from "../generated/schema"
+
+export function handleListCreated(event: ListCreated): void {
+  let list = new List(event.params.list.toString())
+  list.proxy = event.params.proxy
+
+  list.save()
+}
