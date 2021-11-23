4fd08e1a3f1cef2f3fa6a659436a5f31319202e5
David Mihal <david@dmihal.com>
2021-11-23 00:34:05 +0000

    Index adapters added

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,10 +1,30 @@
-// import { BigInt } from "@graphprotocol/graph-ts"
-import { ListCreated } from "../generated/Registry/Registry"
-import { List } from "../generated/schema"
+import { Bytes } from "@graphprotocol/graph-ts"
+import { ListCreated, ElementAdded } from "../generated/Registry/Registry"
+import { List, Adapter, ListAdapter } from "../generated/schema"
+
+function decodeCID(cid: Bytes): string {
+  return Bytes.fromHexString('1220' + cid.toHexString().slice(2)).toBase58()
+}
 
 export function handleListCreated(event: ListCreated): void {
   let list = new List(event.params.list.toString())
   list.proxy = event.params.proxy
 
   list.save()
+}
+
+export function handleElementAdded(event: ElementAdded): void {
+  let adapterCid = decodeCID(event.params.newElement)
+
+  let adapter = Adapter.load(adapterCid)
+  if (!adapter) {
+    adapter = new Adapter(adapterCid)
+  }
+
+  let listAdapter = new ListAdapter(event.params.list.toString() + '-' + adapterCid)
+  listAdapter.list = event.params.list.toString()
+  listAdapter.adapter = adapterCid
+
+  adapter.save()
+  listAdapter.save()
 }
